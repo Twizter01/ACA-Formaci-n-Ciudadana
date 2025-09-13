0d83858738b29603540c1f1819990492cf9f1688
Gabriel Rodriguez <gabrielandrejimenez11@gmail.com>
2025-09-13 22:20:02 +0000

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './App.tsx',
+    './main.tsx',
+    './**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
